37568c91bfd37639f0b428b40ee2721e1785f563
anndrick <anndrick.la@gmail.com>
2022-10-24 16:52:54 -0500

    test

--- src/Components/MainLayout.jsx
+++ src/Components/MainLayout.jsx
@@ -22,6 +22,9 @@ function App() {
                     <li class="nav-item">
                         <a class="nav-link" href="#">Login</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Sign Up</a>
+                    </li>
                 </ul>
                 <form class="d-flex">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
---
+++ src/Screens/SignupScreen.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './SignupScreen.module.css'
+
+function SignupScreen() {
+  console.log('signUp')
+  
+  return (
+    <>
+      <div className="card m-5 justify-center" style={{width: '450px'}}>
+        <div className="text-center fs-1 mt-2 " style={{color: 'red-100'}}>
+          <strong>Registrarse</strong>
+        </div>
+        <div className="card-body">
+          <div className="text-center m-2 mb-5 fs-4">
+            Crea tu cuenta usando el nombre y correo que usas en tu empresa 
+          </div>
+          <form>
+          <div className="mb-3" >
+            <input type="name" placeholder='Nombre' className="form-control" id="name" aria-describedby="CompanyName" style={{height: '60px'}}/>
+          </div>
+          <div className="mb-3">
+            <input type="email" placeholder='Correo' className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" style={{height: '60px'}}/>
+          </div>
+          <div className="mb-3">
+            <input type="password" className="form-control" id="exampleInputPassword1" placeholder='Contraseña' style={{height: '60px'}}/>
+          </div>
+        </form>
+        </div>
+        <div className="text-muted text-center mb-3">
+          <button type="submit" className={`${styles.registerButton} btn`} style={{width: '250px'}}>Aceptar</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default SignupScreen;
