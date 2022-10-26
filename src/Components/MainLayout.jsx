e3110091c646e3d797f95bdcd8b35871fda8df8c
María José Mendoza <a01652137@itesm.mx>
2022-10-25 19:37:25 -0500

    HomeScreen

--- src/Components/MainLayout.jsx
+++ src/Components/MainLayout.jsx
@@ -5,14 +5,14 @@ function App() {
   return (
     <div>
       <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-sm navbar-light bg-light">
             <div class="container-fluid">
                 <img src={logo} width="100" height="60"/>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 justify-content-left">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
@@ -22,13 +22,13 @@ function App() {
                     <li class="nav-item">
                         <a class="nav-link" href="#">Login</a>
                     </li>
-                </ul>
+                  </ul>
                 <form class="d-flex">
-                    <button class="btn btn-outline-info mx-1" type="submit">Home</button>
-                    {/* <input class="form-control mx-1" type="search" placeholder="Search" aria-label="Search"/> */}
-                    <button class="btn btn-outline-info mx-1" type="submit">Profile</button>
-                    <button class="btn btn-outline-info mx-1" type="submit">Login</button>
-                    <button class="btn btn-outline-info mx-1" type="submit">Sign up</button>
+
+                    {/* <button class="btn btn-outline-info mx-1" type="button">Home</button>
+                    <button class="btn btn-outline-info mx-1" type="button">Profile</button>
+                    <button class="btn btn-outline-info mx-1" type="button">Login</button>
+                    <button class="btn btn-outline-info mx-1" type="submit">Sign up</button> */}
 
                 </form>
                 </div>
--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -6,17 +6,17 @@ function HomeScreen() {
   return (
     <div class='container'>
       <div class='hero-unit'>
-        <h1 class= 'mt-5'> Conmutador a asistentes de voz</h1>
-        <p>El nuevo sistema de conmutación automatizado que sea capaz de conectarse a varios motores conversacionales y seleccionar el mejor de acuerdo a la entrada del usuario</p>
+        <h1 class= 'mt-5' style={{color: '#121B4F'}}> Conmutador a asistentes de voz</h1>
+        <p style={{color: '#47525E'}}>El nuevo sistema de conmutación automatizado que sea capaz de conectarse a varios motores conversacionales y seleccionar el mejor de acuerdo a la entrada del usuario</p>
         <p>
         </p>
       </div>
-      <div class='row gx-1 mt-5'>
+      <div class='row gx-3 mt-5 align-center'>
         <div class='col-auto'>
-          <Card border='warning' style={{ width: '15rem', height:'14rem'}} className='border border-4'>
+          <Card style={{ width: '15rem', height:'14rem', borderColor:'#f9e401', borderWidth:'4px'}}>
                 <Card.Body>
-                  <Card.Title>1. Selecciona</Card.Title>
-                  <Card.Text mb-3>
+                  <Card.Title style={{color: '#121B4F'}}>1. Selecciona</Card.Title>
+                  <Card.Text mb-3 style={{color: '#47525E'}}>
                     Las preguntas que más se relacionen a tu negocio, importa documentos csv, añade y edita lo que necesites. 
                   </Card.Text>
                 </Card.Body>
@@ -24,10 +24,10 @@ function HomeScreen() {
               <br />
         </div>
         <div class='col-auto'>
-            <Card border='warning' style={{ width: '15rem', height:'14rem' }} className='border border-4'>
+            <Card style={{ width: '15rem', height:'14rem', borderColor:'#f9e401', borderWidth:'4px'}}>
                   <Card.Body>
-                    <Card.Title >2. Aporta</Card.Title>
-                    <Card.Text mb-3>
+                    <Card.Title style={{color: '#121B4F'}} >2. Aporta</Card.Title>
+                    <Card.Text mb-3 style={{color: '#47525E'}}>
                       Los documentos, respuestas o información relevante relacionada a las preguntas previamente elegidas.
                     </Card.Text>
                   </Card.Body>
@@ -36,20 +36,21 @@ function HomeScreen() {
         </div>
   {/* <div class='w-100'></div> */}
         <div class='col-auto'>
-          <Card border='warning' style={{ width: '15rem', height:'14rem' }} className='border border-4'>
+        <Card style={{ width: '15rem', height:'14rem', borderColor:'#f9e401', borderWidth:'4px'}}>
                 <Card.Body>
-                  <Card.Title>3. Revisa</Card.Title>
-                  <Card.Text mb-3>
+                  <Card.Title style={{color: '#121B4F'}}>3. Revisa</Card.Title>
+                  <Card.Text mb-3 style={{color: '#47525E'}}>
                     El apartado de grabaciones para encontrar todas las llamadas que se generan para conocer mejor a tus clientes. 
                   </Card.Text>
                 </Card.Body>
               </Card>
               <br />
         </div>
-        <div class='col-auto'>  <Card border='warning' style={{ width: '15rem', height:'14rem' }} className='border border-4'>
+        <div class='col-auto'>  
+        <Card style={{ width: '15rem', height:'14rem', borderColor:'#f9e401', borderWidth:'4px'}}>
           <Card.Body>
-            <Card.Title>4. Ejecuta</Card.Title>
-            <Card.Text mb-3>
+            <Card.Title style={{color: '#121B4F'}}>4. Ejecuta</Card.Title>
+            <Card.Text mb-3 style={{color: '#47525E'}}>
               Con la información seleccionada el en botón de generar algoritmo para que el NLP corra con tus reglas.  
             </Card.Text>
           </Card.Body>
